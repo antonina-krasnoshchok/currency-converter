4a5da2921ce62d3949a4cc2894912ba7ae401326
sbaidachni <antonina.krasnoshchok@gmail.com>
2019-10-02 01:10:16 +0300

    added enzyme test

--- src/modules/converter/components/Converter/Converter.test.js
+++ src/modules/converter/components/Converter/Converter.test.js
@@ -1,19 +1,70 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
 import Converter from './index';
+import configureMockStore from 'redux-mock-store';
+
+import NumberFormat from 'react-number-format';
+import Select from '@material-ui/core/Select';
+import IconButton from '@material-ui/core/IconButton';
+
+const storeObj = {
+  converter:{
+    fromAmount: 100,
+    fromCurrency: 'USD',
+    toCurrency: '',
+    rates: {}
+  },
+  chart: {
+    filter: 3
+  }
+}
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('Converter component', () => {
-    const container = shallow(<Converter />);
+    const store = mockStore(storeObj);
+    const container = mount(<Provider store={store}><Converter /></Provider>);
 
-    it('should set the fromAmount value on change', () => {
-        container.find('input[name="fromAmount"]').simulate('change', {
-          target: {
-            value: 100,
-          },
-        });
-        expect(container.find('input[name="fromAmount"]').prop('value')).toEqual(
-          100,
-        );
+    it('should have NumberFormat component', () => {
+      expect(container.find(NumberFormat).length).toEqual(1);
+    });
+
+    it('should set the fromAmount value on change event', () => {
+      container.find(NumberFormat).simulate('change', {
+        target: {
+          value: storeObj.converter.fromAmount,
+        },
       });
-    
+      expect(container.find(NumberFormat).prop('value')).toEqual(storeObj.converter.fromAmount);
+    });
+
+    it('should have Select components', () => {
+      expect(container.find(Select).length).toEqual(2);
+    });
+
+    it('should set the fromCurrency value on change event', () => {
+      container.find('input#fromCurrency').simulate('change', {
+        target: {
+          name: 'fromCurrency',
+          value: storeObj.converter.fromCurrency,
+        },
+      });
+      expect(container.find('input#fromCurrency').prop('value')).toEqual(storeObj.converter.fromCurrency);
+    });
+
+    it('should set the toCurrency value on change event', () => {
+      container.find('input#toCurrency').simulate('change', {
+        target: {
+          name: 'toCurrency',
+          value: storeObj.converter.toCurrency,
+        },
+      });
+      expect(container.find('input#toCurrency').prop('value')).toEqual(storeObj.converter.toCurrency);
+    });
+
+    it('should have Button component', () => {
+      expect(container.find(IconButton).length).toEqual(1);
+    });
 });
